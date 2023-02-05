2bc3a753ff6c4de377f0e8aa8dbe542f4be27258
meteor <songge@gmail.com>
2023-02-05 21:33:29 +0800

    Create std.m

---
+++ std.m
@@ -0,0 +1,10 @@
+@class lean;    // using lean prover to challenge
+@protocol print;    // stdio
+struct math;    // bring us here.
+_Bool b;    // b for best
+int i;  // i for integer
+char c; // c for const
+signed s; // s for string
+float f; // f for function
+long lamba; // lamda expression
+const char C = 1;   // melody
